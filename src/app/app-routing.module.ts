27b9437375b0610b06227e07b30a3e0e27c884dc
Daniela Romero <danyrp45@gmail.com>
2022-11-29 10:56:01 -0600

    add link to new videogame form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConsolasMainPageComponent } from './modules/consolas/consolas-main-page/consolas-main-page.component';
 import { DevelopersMainPageComponent } from './modules/developers/developers-main-page/developers-main-page.component';
+import { VideogameFormComponent } from './modules/videogame/videogame-form/videogame-form.component';
 import { VideogameMainPageComponent } from './modules/videogame/videogame-main-page/videogame-main-page.component';
 
 const routes: Routes = [
   { path: '', component:VideogameMainPageComponent},
   { path: 'desarrolladores', component: DevelopersMainPageComponent },
   { path: 'consolas', component: ConsolasMainPageComponent },
+  { path: 'videojuego/agregar', component: VideogameFormComponent },
 ];
 
 @NgModule({
